dde99c67fa569c69247ee3c506b3cc22a4761bed
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-04-26 15:17:38 -0300

    Feito o exercicio 3 e ajustado o 1

--- bloco_4/dia_3/exercicio_1.js
+++ bloco_4/dia_3/exercicio_1.js
@@ -1,10 +1,10 @@
 // Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
 
 let n = 90;
-let stars = ""
+let stars = "";
 
 for (let i = 0; i < n; i += 1) {
-    stars += "*"
+    stars += "*";
 }
 
 if (n > 1) {
