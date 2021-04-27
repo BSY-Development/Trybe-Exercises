03bce5c62284f91c13c2945de003f464404609bb
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-04-27 19:07:14 -0300

    Apenas alguns testes feitos

--- bloco_4/dia_4/exercicio_bonus_1.js
+++ bloco_4/dia_4/exercicio_bonus_1.js
@@ -2,15 +2,15 @@
 
 function romanoInt(value) {
     let romanos = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
-    let intVal = 0;
+    let result = 0;
     for (let i = 0; i < value.length; i += 1) {
         if ((i > 0) && (romanos[value[i]] > romanos[value[i - 1]])) {
-            intVal += romanos[value[i]] - (2 * romanos[value[i - 1]]);
+            result += romanos[value[i]] - (2 * romanos[value[i - 1]]);
         } else {
-            intVal += romanos[value[i]];
+            result += romanos[value[i]];
         }
     }
-    return intVal;
+    return result;
 }
 
-console.log(romanoInt("MMXXI"));
+console.log(romanoInt("MCMXCVI"));
