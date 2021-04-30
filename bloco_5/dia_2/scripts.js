e3a6cbc1947039c39fa0f7049928219cc13724bc
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-04-30 19:01:37 -0300

    Feito o exercicio 7

--- bloco_5/dia_2/scripts.js
+++ bloco_5/dia_2/scripts.js
@@ -1,33 +1,37 @@
 let bodyVar = document.body;
 
-// Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+// 1- Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
 let title = document.createElement('h1');
 bodyVar.appendChild(title);
 title.innerHTML = "Exercício 5.2 - JavaScript DOM";
 
-// Adicione a tag div com a classe main-content como filho da tag body ;
+// 2- Adicione a tag div com a classe main-content como filho da tag body ;
 let divElement = document.createElement('div');
 bodyVar.appendChild(divElement);
 divElement.classList.add('main-content');
 
-// Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+// 3- Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
 let divSecond = document.createElement('div');
 divElement.appendChild(divSecond);
 divSecond.classList.add('center-content');
 
-// Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
+// 4- Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 let firstP = document.createElement('p');
 divSecond.appendChild(firstP);
 firstP.innerHTML = 'algum texto';
 
-// Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
+// 5- Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 let divThird = document.createElement('div');
 divElement.appendChild(divThird);
 divThird.classList.add('left-content');
 
-// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
+// 6- Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
 let divFourth = document.createElement('div');
 divElement.appendChild(divFourth);
 divFourth.classList.add('right-content');
 
-// 
+// 7- Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
+let imageLeft = document.createElement('img');
+divThird.appendChild(imageLeft);
+imageLeft.classList.add('small-image');
+imageLeft.src = 'https://picsum.photos/200';
