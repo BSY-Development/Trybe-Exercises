a1780e4c189f1bbdde51328fc43fb021576ddf2a
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-21 19:22:04 -0300

    Atividade 2

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -1,22 +1,51 @@
+// const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+
+// // Teste se o retorno de sum(4, 5) é 9
+// assert.strictEqual(sum(4, 5), 9, "4 + 5 = 9");
+
+// // Teste se o retorno de sum(0, 0) é 0
+// assert.strictEqual(sum(0, 0), 0, "0 + 0 = 0");
+
+// // Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+// assert.strictEqual(sum(4, '5'), 9, "4 + 5 = 9");
+// /* Apresenta o erro */
+// // Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+// /* Sim, apresenta o erro programado parametes must be numbers */
+
+/* ------------------------------------------------------------------------------------------------ */
+
 const assert = require('assert');
 
-function sum(a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number') {
-    throw new Error('parameters must be numbers');
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
   }
-
-  return a + b;
+  return newArr;
 }
 
-// Teste se o retorno de sum(4, 5) é 9
-assert.strictEqual(sum(4, 5), 9, "4 + 5 = 9");
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// console.log(myRemove([1, 2, 3, 4], 2));
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 4), [1, 2, 3], 'Removido o 3');
 
-// Teste se o retorno de sum(0, 0) é 0
-assert.strictEqual(sum(0, 0), 0, "0 + 0 = 0");
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Diferente de [1, 2, 3, 4]');
 
-// Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
-assert.strictEqual(sum(4, '5'), 9, "4 + 5 = 9");
-/* Apresenta o erro */
-// Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
-/* Sim, apresenta o erro programado parametes must be numbers */
+// Verifique se o array passado por parâmetro não sofreu alterações
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4], 'Não mudou nada.');
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4], 'Removido o 5');
+
+/* ------------------------------------------------------------------------------------------------ */
 
