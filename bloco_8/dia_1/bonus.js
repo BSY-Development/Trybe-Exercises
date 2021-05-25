6fd2304e7b257da2f3cfa8b21e0b5d3c94206ce0
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-25 19:44:30 -0300

    Exercicios Bonus Feito

--- bloco_8/dia_1/bonus.js
+++ bloco_8/dia_1/bonus.js
@@ -1,21 +1,21 @@
 const mage = {
   healthPoints: 130,
   intelligence: 45,
-  mana: 125,
-  damage: undefined,
+  mana: 60,
+  damage: 0,
 };
 
 const warrior = {
   healthPoints: 200,
   strength: 30,
   weaponDmg: 2,
-  damage: undefined,
+  damage: 0,
 };
 
 const dragon = {
-  healthPoints: 350,
+  healthPoints: 500,
   strength: 50,
-  damage: undefined,
+  damage: 0,
 };
 
 const battleMembers = {
@@ -25,7 +25,7 @@ const battleMembers = {
 };
 
 // Requisito 1
-const dragonAttack = (dragon) => {
+const dragonAttack = () => {
   let minimalDamage = 15;
   let maximumDamage = dragon.strength;
   let damage = Math.ceil(Math.random() * (maximumDamage - (minimalDamage - 1)) + (minimalDamage - 1));
@@ -34,7 +34,7 @@ const dragonAttack = (dragon) => {
 }
 
 // Requisito 2
-const warriorAttack = (warrior) => {
+const warriorAttack = () => {
   let minimalDamage = warrior.strength;
   let maximumDamage = minimalDamage * warrior.weaponDmg;
   let damage = Math.ceil(Math.random() * (maximumDamage - (minimalDamage - 1)) + (minimalDamage - 1));
@@ -43,14 +43,57 @@ const warriorAttack = (warrior) => {
 }
 
 // Requisito 3
-const mageAttack = (mage) => {
+const mageAttack = () => {
   if (mage.mana < 15) {
-    return 'Não possui mana suficiente';
+    return 0;
   }
   mage.mana -= 15;
   let minimalDamage = mage.intelligence;
   let maximumDamage = minimalDamage * 2;
   damage = Math.ceil(Math.random() * (maximumDamage - (minimalDamage - 1)) + (minimalDamage - 1));
 
-  return { dano: damage, mana: mage.mana }
+  return damage;
+}
+
+const gameActions = (attacker, hit) => {
+  let damageDealt = hit();
+  attacker.damage += damageDealt;
+  if (attacker === dragon) {
+    mage.healthPoints -= damageDealt;
+    warrior.healthPoints -= damageDealt;
+  } else {
+    dragon.healthPoints -= damageDealt;
+  }
+};
+
+const gameOver = () => {
+  if (mage.healthPoints <= 0 && warrior.healthPoints <= 0) {
+    console.log('Combate acabou, o dragão venceu.');
+    return true;
+  } else if (dragon.healthPoints <= 0) {
+    console.log(battleMembers);
+    console.log('Combate acabou, você venceu!');
+    return true;
+  }
+  return false;
+}
+
+let isOver = false;
+while(!isOver) {
+
+  if (mage.healthPoints > 0) {
+    gameActions(mage, mageAttack);
+  }
+
+  if (warrior.healthPoints > 0) {
+    gameActions(warrior, warriorAttack);
+  }
+
+  if (gameOver()) {
+    break;
+  }
+
+  gameActions(dragon, dragonAttack);
+  console.log(battleMembers);
+  isOver = gameOver();
 }
