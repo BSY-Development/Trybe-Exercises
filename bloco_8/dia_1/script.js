c46cd567db5a324914564c9a8ba8fcb935cdd269
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-25 17:22:47 -0300

    Exercicio 2 feito

--- bloco_8/dia_1/script.js
+++ bloco_8/dia_1/script.js
@@ -1,3 +1,5 @@
+/* Exercicio 1 */
+
 const newEmployees = (func) => {
   const employees = {
     id1: func('Pedro Guerra'),
@@ -16,4 +18,20 @@ const createObj = (nome) => {
   return items;
 }
 
-newEmployees(createObj);
+// newEmployees(createObj);
+
+/* Exercicio 2 */
+
+const checkWin = (numGuessed, numRandom) => {
+  if (numGuessed === numRandom) {
+    return 'Parabéns você ganhou';
+  }
+  return 'Tente novamente';
+}
+
+const result = (func, num) => {
+  let randomNumber = Math.ceil(Math.random() * 5);
+  return func(num, randomNumber);
+}
+
+console.log(result(checkWin, 1));
