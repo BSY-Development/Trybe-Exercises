dda280cd20490f8c682a13a8ebb677f2b149edd9
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-25 18:14:47 -0300

    Feito a parte 1 do jogo - ataque do dragao

---
+++ bloco_8/dia_1/bonus.js
@@ -0,0 +1,35 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = {
+  mage,
+  warrior,
+  dragon,
+};
+
+// Requisito 1
+const dragonAttack = (dragon) => {
+  let minimalDamage = 15;
+  let dragonStrength = dragon.strength;
+  let damage = Math.ceil(Math.random() * (dragonStrength - (minimalDamage - 1)) + (minimalDamage - 1));
+  
+  return damage;
+}
+
