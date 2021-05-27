ff445ba6aa6df3fc06e21de39ac0ec288315f2b2
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-27 19:03:16 -0300

    Exercicio 5

--- bloco_8/dia_3/exercise5.js
+++ bloco_8/dia_3/exercise5.js
@@ -1,2 +1,20 @@
 const assert = require('assert');
 const books = require('./obj');
+
+const expectedResult = [
+  'Frank Herbert',
+  'George R. R. Martin',
+  'Isaac Asimov',
+  'J. R. R. Tolkien',
+];
+
+// Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+function fantasyOrScienceFictionAuthors() {
+  const livros = books.filter((item) => (item.genre === 'Ficção Científica' || item.genre === 'Fantasia'));
+  const names = livros.map((item) => item.author.name);
+  names.sort();
+  return names;
+}
+
+assert.deepStrictEqual(fantasyOrScienceFictionAuthors(), expectedResult);
