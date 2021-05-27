58e6c2304e3326777b2d2ccfc8e0a6f8f4dd4f40
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-27 19:20:19 -0300

    Exercicio 7

--- bloco_8/dia_3/exercise7.js
+++ bloco_8/dia_3/exercise7.js
@@ -1,2 +1,13 @@
 const assert = require('assert');
 const books = require('./obj');
+
+const expectedResult = 'O Senhor dos Anéis';
+
+// Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+// Dica: cada inicial termina com um ponto.
+// 7
+function authorWith3DotsOnName() {
+  return books.find((item) => item.author.name.startsWith('.', 7)).name;
+}
+
+assert.deepStrictEqual(authorWith3DotsOnName(), expectedResult);
