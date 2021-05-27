fccab6fb946c66ec45f957f5b03fdf26a10233a3
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-27 19:23:12 -0300

    removido a constante desnecessaria da funcao

--- bloco_8/dia_3/exercise1.js
+++ bloco_8/dia_3/exercise1.js
@@ -5,16 +5,16 @@ const books = require('./obj');
 // Dica: Use a função map
 
 const expectedResult = [
-    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-    'Fundação - Ficção Científica - Isaac Asimov',
-    'Duna - Ficção Científica - Frank Herbert',
-    'A Coisa - Terror - Stephen King',
-    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
-  ];
-  
-  function formatedBookNames() {
-    return books.map((item) => `${item.name} - ${item.genre} - ${item.author.name}`);
-  }
-  
-  assert.deepStrictEqual(formatedBookNames(), expectedResult);
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formatedBookNames() {
+  return books.map((item) => `${item.name} - ${item.genre} - ${item.author.name}`);
+}
+
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
