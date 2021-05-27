30be6ad21cc1c31bd1fa9bf3d3b01f5aba338a24
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-27 18:27:42 -0300

    Atividade 2

--- bloco_8/dia_3/exercise1.js
+++ bloco_8/dia_3/exercise1.js
@@ -4,6 +4,18 @@ const books = require('./obj');
 // Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
 // Dica: Use a função map
 
-const formatacao = books.map((item) => `${item.name} - ${item.genre} - ${item.author.name}`);
-
-console.log(formatacao);
+const expectedResult = [
+    'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+    'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+    'Fundação - Ficção Científica - Isaac Asimov',
+    'Duna - Ficção Científica - Frank Herbert',
+    'A Coisa - Terror - Stephen King',
+    'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  ];
+  
+  function formatedBookNames() {
+    const formatacao = books.map((item) => `${item.name} - ${item.genre} - ${item.author.name}`);
+    return formatacao;
+  }
+  
+  assert.deepStrictEqual(formatedBookNames(), expectedResult);
--- bloco_8/dia_3/exercise2.js
+++ bloco_8/dia_3/exercise2.js
@@ -1,2 +1,42 @@
 const assert = require('assert');
 const books = require('./obj');
+
+// Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , 
+// com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi 
+// lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha 
+// considerando suas idades quando o livro foi lançado.
+// Dica: use as funções map , sort
+
+const expectedResult = [{
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+
+function nameAndAge() {
+  const obj = books.map((item) => ({age: (item.releaseYear - item.author.birthYear), author: item.author.name}));
+  obj.sort((a, b) => a.age - b.age);
+  return obj;
+}
+
+assert.deepStrictEqual(nameAndAge(), expectedResult);
