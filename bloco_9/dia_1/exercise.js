b6fe223bbc8f9ed82b884d802ba65cd2a2a9fd31
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-07 18:34:03 -0300

    Exercicio 4

--- bloco_9/dia_1/exercise.js
+++ bloco_9/dia_1/exercise.js
@@ -25,15 +25,31 @@ const jupiter = {
   },
 };
 
-const getPlanet = () => {
-  const mars = {
-    name: "Mars",
-    distanceFromSun: {
-      value: 227900000,
-      measurementUnit: "kilometers",
-    },
-  };
-  setTimeout(() => console.log("Returned planet: ", mars), 4000); // imprime Marte depois de 4 segundos
+/* Exercicio 3 */
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   setTimeout(() => console.log("Returned planet: ", mars), 4000); // imprime Marte depois de 4 segundos
+// };
+
+// getPlanet();
+
+/* Exercicio 4 */
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
 };
 
-getPlanet();
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), messageDelay());
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
