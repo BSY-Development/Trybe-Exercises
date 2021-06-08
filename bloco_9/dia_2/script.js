891fa59f2bb7dd61bbd623ef2db2be14287d6a30
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-08 18:45:39 -0300

    Exercicio 2

--- bloco_9/dia_2/script.js
+++ bloco_9/dia_2/script.js
@@ -4,14 +4,29 @@ const joke = document.querySelector('#jokeContainer');
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  };
-
-  fetch(API_URL, myObject)
+  fetch(API_URL, { headers: { Accept: 'application/json' } })
     .then(response => response.json())
-    .then(data => joke.innerHTML = data.joke);
+    .then(data =>
+      joke.innerHTML = data.joke
+    );
 };
 
 window.onload = () => fetchJoke();
+
+const less8000 = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const arr = [];
+    for (let i = 0; i < 10; i += 1) {
+      arr.push(Math.pow(Math.ceil(Math.random() * 50), 2));
+    }
+    const sum = arr.reduce((acc, item) => acc + item, 0);
+    // console.log(sum);
+    (sum < 8000) ? resolve() : reject('Promise rejeitada');
+  })
+
+  myPromise
+  .then(() => console.log('Promise resolvida'))
+  .catch((error) => console.log(error));
+}
+
+less8000();
