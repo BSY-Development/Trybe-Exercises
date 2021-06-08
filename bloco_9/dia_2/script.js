f28a121eee9b9c98bd4f7bf37364db21c4993555
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-08 19:28:14 -0300

    Requisito Bonus

--- bloco_9/dia_2/script.js
+++ bloco_9/dia_2/script.js
@@ -1,10 +1,13 @@
 const joke = document.querySelector('#jokeContainer');
 
-// apiScript.js     
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  fetch(API_URL, { headers: { Accept: 'application/json' } })
+  fetch(API_URL, {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
     .then(response => response.json())
     .then(data =>
       joke.innerHTML = data.joke
@@ -13,21 +16,50 @@ const fetchJoke = () => {
 
 window.onload = () => fetchJoke();
 
+/* Then */
+// const less8000 = () => {
+//   const myPromise = new Promise((resolve, reject) => {
+//     const arr = [];
+//     for (let i = 0; i < 10; i += 1) {
+//       arr.push(Math.pow(Math.ceil(Math.random() * 50), 2));
+//     }
+//     const sum = arr.reduce((acc, item) => acc + item, 0);
+//     // console.log(sum);
+//     (sum < 8000) ? resolve(sum) : reject();
+//   })
+
+//   myPromise
+//   .then((sum) => [2, 3, 5, 10].map((item) => sum / item))
+//   .then((array) => array.reduce((acc, item) => acc + item, 0))
+//   .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+// }
+
+// less8000();
+
+/* Async await */
+
+const createArray = () => {
+  return Array.from({ length: 10 }, () => Math.pow(Math.ceil(Math.random() * 50), 2))
+    .reduce((acc, item) => acc + item, 0);
+}
+
 const less8000 = () => {
+
   const myPromise = new Promise((resolve, reject) => {
-    const arr = [];
-    for (let i = 0; i < 10; i += 1) {
-      arr.push(Math.pow(Math.ceil(Math.random() * 50), 2));
-    }
-    const sum = arr.reduce((acc, item) => acc + item, 0);
-    // console.log(sum);
-    (sum < 8000) ? resolve(sum) : reject();
+    const sum = createArray();
+    (sum < 8000) ? resolve(sum): reject('É mais de oito mil! Essa promise deve estar quebrada!');
   })
 
-  myPromise
-  .then((sum) => [2, 3, 5, 10].map((item) => sum / item))
-  .then((array) => array.reduce((acc, item) => acc + item, 0))
-  .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
+  const fetchPromise = async () => {
+    try {
+      const res = await myPromise;
+      [2, 3, 5, 10].map((item) => res / item).reduce((acc, item) => acc + item, 0);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  fetchPromise();
 }
 
 less8000();
