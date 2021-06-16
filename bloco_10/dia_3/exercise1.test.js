d4405e8bc8180cb92c51607158bf4fc75a08cd70
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-16 18:51:54 -0300

    Exercicio 3

--- bloco_10/dia_3/exercise1.test.js
+++ bloco_10/dia_3/exercise1.test.js
@@ -16,3 +16,23 @@ test('Faz os testes do exercicio 2', () => {
   expect(generateRandomNumber).toBeCalledTimes(1);
   expect(generateRandomNumber).toBeCalledWith(10, 5);
 });
+
+test('Faz os testes do exercicio 3', () => {
+  /* Faz a primeira parte do exercicio, multiplicando 3 números */
+  generateRandomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
+  
+  expect(generateRandomNumber(10, 5, 2)).toBe(100);
+  expect(generateRandomNumber).toBeCalledTimes(1);
+  expect(generateRandomNumber).toBeCalledWith(10, 5, 2);
+
+  /* Reseta o mock */
+  generateRandomNumber.mockReset();
+  expect(generateRandomNumber).toBeCalledTimes(0);
+
+  /* Faz a segunda parte do exercicio, retornando o dobro do número passado */
+  generateRandomNumber = jest.fn().mockImplementation((a) => a * 2);
+
+  expect(generateRandomNumber(10)).toBe(20);
+  expect(generateRandomNumber).toBeCalledTimes(1);
+  expect(generateRandomNumber).toBeCalledWith(10);
+});
