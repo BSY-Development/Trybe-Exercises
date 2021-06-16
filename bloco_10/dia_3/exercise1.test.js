200e781ed100557b79a1c590b0dd6e3f0f97f244
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-06-16 18:45:28 -0300

    Exercicio 2

--- bloco_10/dia_3/exercise1.test.js
+++ bloco_10/dia_3/exercise1.test.js
@@ -4,8 +4,15 @@ test('Faz os testes do exercicio 1', () => {
   generateRandomNumber = jest.fn().mockReturnValue(10);
 
   generateRandomNumber();
-  expect(generateRandomNumber).toBeCalled();
   expect(generateRandomNumber).toBeCalledTimes(1);
   expect(generateRandomNumber()).toBe(10);
   expect(generateRandomNumber).toBeCalledTimes(2);
+});
+
+test('Faz os testes do exercicio 2', () => {
+  generateRandomNumber = jest.fn().mockImplementation((a, b) => a / b);
+
+  expect(generateRandomNumber(10, 5)).toBe(2);
+  expect(generateRandomNumber).toBeCalledTimes(1);
+  expect(generateRandomNumber).toBeCalledWith(10, 5);
 });
