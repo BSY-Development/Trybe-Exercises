a4cb8f3d880e012f361b9b78e0e3ec929d6cb09e
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-07-27 23:50:24 -0300

    Criado uma forma de criar o cadastro do usuario

--- bloco_16/dia_3/login-system/src/pages/Cadastro.js
+++ bloco_16/dia_3/login-system/src/pages/Cadastro.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { actionCadastrar } from '../redux/actions/index';
 
-export default class Cadastro extends React.Component {
+class Cadastro extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      age: 0,
+      password: '',
+    }
+
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler({target}) {
+    this.setState({
+      [target.name]: target.value,
+    });
+  }
+
   render() {
+    const { name, email, age, password } = this.state;
+    const { signUp } = this.props;
     return (
       <fieldset>
         <form>
-          
+          <label>
+            Nome:
+            <input onChange={ this.changeHandler } value={ name } name="name" type="text" required />
+          </label>
+          <br />
+          <label>
+            E-mail:
+            <input onChange={ this.changeHandler } value={ email } name="email" type="email" required />
+          </label>
+          <br />
+          <label>
+            Idade:
+            <input onChange={ this.changeHandler } value={ age } name="age" type="number" required />
+          </label>
+          <br />
+          <label>
+            Senha:
+            <input onChange={ this.changeHandler } value={ password } name="password" type="password" required />
+          </label>
+          <button type="button" onClick={ () => signUp(this.state) }>Cadastrar</button>
         </form>
       </fieldset>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  signUp: (state) => dispatch(actionCadastrar(state)),
+});
+
+export default connect(null, mapDispatchToProps)(Cadastro);
--- bloco_16/dia_3/login-system/src/redux/reducers/index.js
+++ bloco_16/dia_3/login-system/src/redux/reducers/index.js
@@ -1,12 +1,13 @@
 const INITIAL_STATE = {
   users: [],
+  user: {},
   isLogged: false,
 };
 
 const reducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'ACTION_CADASTRAR':
-      return { ...state, users: action.payload };
+      return { ...state, users: state.users.concat(action.payload) };
     default:
       return state;
   }
