a727bdf5bb33da9413342692843cc251869336ae
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-16 15:19:20 -0300

    Feito outros 3 requisitos

--- bloco_21/dia_1/exercise.sql
+++ bloco_21/dia_1/exercise.sql
@@ -5,9 +5,9 @@ SELECT MAX(SALARY) - MIN(SALARY) FROM employees;
 -- 3. Escreva uma query que exiba a média salarial de cada JOB_ID , ordenando pela média salarial em ordem decrescente.
 SELECT JOB_ID, AVG(SALARY) FROM employees GROUP BY JOB_ID ORDER BY SALARY DESC;
 -- 4. Escreva uma query que exiba a quantidade de dinheiro necessária para realizar o pagamento de todas as pessoas funcionárias.
-
+SELECT SUM(SALARY) FROM employees;
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
-
+SELECT MAX(SALARY), MIN(SALARY), SUM(SALARY), ROUND(AVG(SALARY), 2) FROM employees;
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
 
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
