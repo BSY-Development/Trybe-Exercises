9f47a6d49d63a3c4f0f017f1494de4f8367e23e7
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-16 15:22:37 -0300

    Feito mais 3, ate o 9

--- bloco_21/dia_1/exercise.sql
+++ bloco_21/dia_1/exercise.sql
@@ -9,13 +9,13 @@ SELECT SUM(SALARY) FROM employees;
 -- 5. Escreva uma query que exiba quatro informações: o maior salário, o menor salário, a soma de todos os salários e a média dos salários. Todos os valores devem ser formatados para ter apenas duas casas decimais.
 SELECT MAX(SALARY), MIN(SALARY), SUM(SALARY), ROUND(AVG(SALARY), 2) FROM employees;
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras ( IT_PROG ).
-
+SELECT COUNT(*) FROM employees WHERE JOB_ID = 'IT_PROG';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão ( JOB_ID ).
-
+SELECT JOB_ID, SUM(SALARY) FROM employees GROUP BY JOB_ID;
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras ( IT_PROG ).
-
+SELECT JOB_ID, SUM(SALARY) FROM employees WHERE JOB_ID = 'IT_PROG' GROUP BY JOB_ID;
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras ( IT_PROG ).
-
+SELECT JOB_ID, AVG(SALARY) FROM employees WHERE JOB_ID != 'IT_PROG' GROUP BY JOB_ID;
 -- 10. Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo department_id .
 
 -- 11. Escreva uma query que atualize a coluna PHONE_NUMBER , de modo que todos os telefones iniciados por 515 agora devem iniciar com 777 .
