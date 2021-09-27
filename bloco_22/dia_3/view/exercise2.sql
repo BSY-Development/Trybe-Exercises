a92320b370690b77e2687e62a49df5021c2c9d19
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-09-27 15:59:18 -0300

    Exercicio 2 - View

---
+++ bloco_22/dia_3/view/exercise2.sql
@@ -0,0 +1,19 @@
+/* Crie uma view chamada film_info utilizando as tabelas actor , film_actor e film do banco de dados sakila. 
+Sua view deve exibir o actor_id , o nome completo do ator ou da atriz em uma coluna com o ALIAS actor 
+e o título dos filmes. Os resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem 
+a seguir como referência. */
+
+CREATE VIEW film_info AS
+	SELECT 
+		fa.actor_id,
+		CONCAT(a.first_name, ' ', a.last_name) AS `actor`,
+		f.title
+	FROM
+		film_actor fa
+			INNER JOIN
+		actor a ON a.actor_id = fa.actor_id
+			INNER JOIN
+		film f ON fa.film_id = f.film_id
+	ORDER BY `actor`;
+
+SELECT * FROM film_info;
